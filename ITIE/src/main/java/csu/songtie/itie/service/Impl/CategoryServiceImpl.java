a918ab9fb96236f78f298c3cb75f983b80bd682c
limoli2024 <3240740065@qq.com>
2025-05-25 16:50:24 +0800

    响应码

---
+++ ITIE/src/main/java/csu/songtie/itie/service/Impl/CategoryServiceImpl.java
@@ -0,0 +1,20 @@
+package csu.songtie.itie.service.Impl;
+
+import csu.songtie.itie.domain.entity.Category;
+import csu.songtie.itie.mapper.CategoryMapper;
+import csu.songtie.itie.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CategoryServiceImpl implements CategoryService {
+    @Autowired
+    private CategoryMapper categoryMapper;
+
+    @Override
+    public List<Category> getCategoryList() {
+        return categoryMapper.selectList(null);
+    }
+}
